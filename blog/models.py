713a0322907ec88c83a6dd917fb734e0a7c789e1
alif-0404 <alifjonbekov@icloud.com>
2025-04-29 14:54:45 +0500

    forms and user accounts

--- blog/models.py
+++ blog/models.py
@@ -2,10 +2,11 @@ from django.db import models
 from django.urls import reverse 
 
 class Post(models.Model):
+    admin = 'ADMIN'
     title = models.CharField(max_length=200)
     author = models.ForeignKey (
-        'auth.User' ,
-        on_delete=models.CASCADE , 
+        'auth.User',
+        on_delete=models.CASCADE ,     
     )
 
     body = models.TextField()
