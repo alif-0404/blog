9badddfe55b32e48c7a593978d4da360b5c157bc
alif-0404 <alifjonbekov@icloud.com>
2025-12-05 09:47:36 +0500

    blog

--- blog/models.py
+++ blog/models.py
@@ -10,6 +10,11 @@ class Post(models.Model):
         on_delete=models.CASCADE,
     )
     body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at']
 
     def __str__(self):
         return self.title
